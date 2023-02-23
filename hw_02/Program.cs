84808a8edf7e2fb85e01b56cb8c77d7f71bb2182
Natalyae <ngk.s@mail.ru>
2023-02-23 07:53:10 +0100

    Add code hw02

--- hw_02/Program.cs
+++ hw_02/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Write("Введите координаты первой точки (xа ≠ 0, yа ≠ 0, zа ≠ 0): ");
+int xa = Convert.ToInt32(Console.ReadLine());
+int ya = Convert.ToInt32(Console.ReadLine());
+int za = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты второй точки (xb ≠ 0, yb ≠ 0, zа ≠ 0): ");
+int xb = Convert.ToInt32(Console.ReadLine());
+int yb = Convert.ToInt32(Console.ReadLine());
+int zb = Convert.ToInt32(Console.ReadLine());
+
+int x = xb-xa;
+int y = yb-ya;
+int z = zb-za;
+double dist = Math.Sqrt(Math.Pow(x,2)+Math.Pow(y,2)+Math.Pow(z,2));
+Console.Write(dist);
+
